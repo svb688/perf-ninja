6730b49cfdebbb76cfed03b1bf5bba70fdd064b9
jwest591 <jwest591@users.noreply.github.com>
2021-08-12 11:02:52 +0100

    Confirm sizeof(S) == 8 with static_assert

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -18,6 +18,8 @@ struct S {
   bool operator<(const S &s) const { return this->i < s.i; }
 };
 
+static_assert(sizeof(S) == 8);
+
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
 void solution(std::array<S, N> &arr);
