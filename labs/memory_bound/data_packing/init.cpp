5759e1a5d9d5d995448916c88fc037346308d5e8
Rasmus Bons <rasmusbons@gmail.com>
2021-08-15 08:19:52 +0200

    switched over to use bitfield

--- labs/memory_bound/data_packing/init.cpp
+++ labs/memory_bound/data_packing/init.cpp
@@ -2,17 +2,20 @@
 #include "solution.h"
 #include <random>
 
-void init(std::array<S, N> &arr) {
-  std::default_random_engine generator;
-  std::uniform_int_distribution<uint8_t> distribution(minRandom, maxRandom - 1);
+void init(std::array<S, N> &arr)
+{
+    std::default_random_engine generator;
+    std::uniform_int_distribution<uint8_t> distribution(minRandom, maxRandom - 1);
 
-  for (int i = 0; i < N; i++) {
-      uint8_t random_int1 = distribution(generator);
-      uint8_t random_int2 = distribution(generator);
+    for (int i = 0; i < N; i++)
+    {
+        uint8_t random_int1 = distribution(generator);
+        uint8_t random_int2 = distribution(generator);
 
-    arr[i].i = random_int1; // only need to be 8 bit
-    arr[i].s = random_int2 | ((random_int1 < random_int2) << 7); // 8 bits as well
-    arr[i].l = random_int1 * random_int2; // 99 * 99 = 9801 need to be short
-    arr[i].d = static_cast<float>(random_int1) / maxRandom; // could be downgrade to float
-  }
+        arr[i].i = random_int1; // only need to be 8 bit
+        arr[i].s = random_int2; // bit shift in the original b
+        arr[i].l = random_int1 * random_int2; // 99 * 99 = 9801 need to be short
+        arr[i].b = random_int1 < random_int2;
+        arr[i].d = static_cast<float>(random_int1) / maxRandom; // could be downgrade to float
+    }
 }
--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -10,10 +10,11 @@ constexpr int maxRandom = 100;
 // FIXME: this data structure can be reduced in size
 struct S {
     float d; // 32 bit float
-    short l; // 99 * 99 = 9801 -> 15 bits
-    uint8_t i; // 0-99 = 7 bits
-    uint8_t s; // 0-99 = 7 bits
-    // bool b; // bit shift it into s
+    unsigned short l : 15; // 99 * 99 = 9801 -> 15 bits
+    uint8_t i : 7; // 0-99 = 7 bits
+    uint8_t s : 7; // 0-99 = 7 bits
+    bool b : 1; // bit shift it into s
+    // TODO: do float really need to be decimal? It all values will be between 0.0 and 0.99
 
     bool operator<(const S &s) const { return this->i < s.i; }
 };
