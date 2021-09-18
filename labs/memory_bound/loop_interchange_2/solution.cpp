f171af608a5b7e6de47ea20ec1363745d3dd8aa0
afigegoznaet <enrabiar@gmail.com>
2021-09-18 23:42:30 +0300

    loop interchange B 1 for Linux

--- labs/memory_bound/loop_interchange_2/solution.cpp
+++ labs/memory_bound/loop_interchange_2/solution.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <ios>
+#include <string.h>
 
 // Applies Gaussian blur in independent vertical lines
 static void filterVertically(uint8_t *output, const uint8_t *input,
