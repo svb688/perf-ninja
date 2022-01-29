a9414eb9caff8f6c19723ebce99b8affb3e38fe3
Alex Popov <alexgpg@gmail.com>
2022-01-29 10:34:27 +0300

    data packing: Add solution

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -6,16 +6,17 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
-  int i;
-  long long l;
-  short s;
-  double d;
-  bool b;
+  float d;
+  unsigned short l : 14;
+  unsigned short b : 2;
+  unsigned char i;
+  unsigned char s;
 
   bool operator<(const S &s) const { return this->i < s.i; }
 };
+
+static_assert(sizeof(S) == 8);
 
 void init(std::array<S, N> &arr);
 S create_entry(int first_value, int second_value);
